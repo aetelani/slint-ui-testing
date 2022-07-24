d6473a0e0709d44bdb52d178556be66342a3a4ed
Anssi Eteläniemi <anssi.etelaniemi@confidex.com>
2022-07-25 02:37:43 +0300

    Flex style layout test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+// Copyright (C) 2022 Anssi Eteläniemi <aetelani@hotmail.com>
 use slint::{Model, ModelRc, VecModel};
 use slint::{Timer, TimerMode};
 use rusqlite::{Connection, Result};
@@ -6,33 +7,77 @@ slint::slint! {
     import { SpinBox, Button, CheckBox, Slider, LineEdit, ScrollView, ListView,
         HorizontalBox, VerticalBox, StandardButton, GridBox } from "std-widgets.slint";
 
-    export struct TicketItem := {
+    export struct Data := {
+        grid-col: int,
+        grid-row: int,
         uid: string,
     }
 
     MainWindow := Window {
         preferred-width: 400px;
         preferred-height: 600px;
-        property <[TicketItem]> ticket-model: [];
-        callback update-model();
-        update-model() => { it-row = 0; }
-
-        property <int> it-row: 0;
-        property<int> it-col: -1;
-
-        for it[ind] in ticket-model:
+        property <int> grid-col: 0;
+        property <int> grid-row: 0;
+        property <int> grid-growth-max: 5;
+        property <int> grid-row-increase: 0;
+        property <int> grid-col-increase: 0;
+        property <[Data]> model: [];
+        callback inc-col(int, int); inc-col(v, a) => { grid-col = v + a; }
+        callback inc-row(int, int); inc-row(v, a) => { grid-row = v + a; }
+        callback inc-col-sem(int, int); inc-col-sem(v, a) => { grid-col-increase = v + a; }
+        callback inc-row-sem(int, int); inc-row-sem(v, a) => { grid-row-increase = v + a; }
+        callback update-grid(int);
+        update-grid(ind) => {
+            if (ind < grid-growth-max) {
+                grid-col = ind;
+                grid-row = 0;
+                return;
+            } else if (ind == grid-growth-max) {
+                grid-col = 0;
+                grid-row = 1;
+                grid-row-increase = 0;
+                grid-col-increase = 3;
+                return;
+            }
+            if (ind == model.length - 1) {
+                grid-row-increase = 0;
+                grid-col-increase = 0;
+                return; }
+            if (grid-row-increase == 3) {
+                //grid-row += 1;
+                inc-row(grid-row, 1);
+                grid-col = 0;
+                grid-row-increase = 0;
+                grid-col-increase = 0;
+            }
+            if (grid-col-increase == 3) {
+                //grid-col += 1;
+                inc-col(grid-col, 1);
+                grid-col-increase = 0;
+            }
+            // Conditions
+            if (mod(ind, grid-growth-max) == grid-growth-max - 1) {
+                //grid-row-increase += 1;
+                inc-row-sem(grid-row-increase, 1);
+            } else if (ind == model.length - 1) {
+            } else {
+                inc-col-sem(grid-col-increase, 1);
+                //grid-col-increase += 1;
+            }
+        }
+        for it[ind] in model:
             Text {
-                x: { it-col += 1; it-col * 30px }
-                y: {
-                    debug(mod(ind,3));
-                    if (mod(ind, 3) == 0) {
-                        it-row += 1;
-                        it-col = -1;
-                    }
-                    it-row * 20px
-                }
-                text: { if (visible) { it.uid + "v" } else { it.uid + "h" } };
+                x: { update-grid(ind); it.grid-col * 20px } // FIXed: Just use the pre-count values
+                y: { update-grid(ind); it.grid-row * 20px }
+                //text: { "("+grid-col+","+grid-row+")" };
+                text: { model[ind].uid };
                 visible: false;
+                states [
+                    //start-grid when ind == 0: { y: 0; x: 0; }
+                    //last-col when (mod(ind, grid-growth-max) == grid-growth-max - 1): { y: y + 20px; x: 0px; }
+                    //start-col when (mod(ind, grid-growth-max) == 0): { x: 0px; y: y + 20px; }
+                    //next-col when (mod(ind, grid-growth-max) != 0): { x: x + 20px; }
+                ]
             }
     }
 }
@@ -46,14 +91,18 @@ pub fn main() {
     let handle_clone = handle_weak.clone();
     let timer = Timer::default();
     let mut count: usize = 0;
+    let mut row: i32 = 0;
+    let mut col: i32 = 0;
+    let max_growth = 5usize;
     create_tables();
-    timer.start(TimerMode::Repeated, std::time::Duration::from_millis(500), move || {
-        let model_handle = handle_clone.unwrap().get_ticket_model();
-        let model = model_handle.as_any().downcast_ref::<VecModel<TicketItem>>().unwrap();
-        model.push(TicketItem{ uid: format!("{0}", count).into()});
+    timer.start(TimerMode::Repeated, std::time::Duration::from_millis(200), move || {
+        let model_handle: ModelRc<Data> = handle_clone.unwrap().get_model();
+        let model: &VecModel<Data> = model_handle.as_any().downcast_ref::<VecModel<Data>>().unwrap();
+        model.push(Data{ grid_col:col as i32, grid_row: row, uid: format!("{0}", count).into()});
+        if count % max_growth == max_growth - 1 { row += 1; col = 0; }
+        else { col += 1; }
         ticket_encoded(count);
         count += 1;
-        handle_clone.unwrap().invoke_update_model();
     });
     handle.run();
 }
