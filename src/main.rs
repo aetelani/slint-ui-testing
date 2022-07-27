b3ec445715cfcce683f28433f126c7b6c21e04e9
Anssi Eteläniemi <anssi.etelaniemi@confidex.com>
2022-07-27 21:37:27 +0300

    Added Multiselect

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
+use std::ops::{Deref, RangeInclusive};
 // Copyright (C) 2022 Anssi Eteläniemi <aetelani@hotmail.com>
-use slint::{Model, ModelRc, VecModel};
+use slint::{FilterModel, Model, ModelExt, ModelRc, RenderingNotifier, VecModel};
 use slint::{Timer, TimerMode};
 use rusqlite::{Connection, Result};
 
@@ -10,6 +11,7 @@ slint::slint! {
     export struct Data := {
         grid-col: int,
         grid-row: int,
+        selected: bool,
         uid: string,
     }
     Button := Rectangle {
@@ -33,25 +35,41 @@ slint::slint! {
         preferred-width: 400px;
         preferred-height: 600px;
         property <[Data]> model: [];
+        property <int> range-select-started-from: -1;
+        callback range-select(int, int, bool);
         for it[ind] in model:
             rect := Rectangle {
+                property <bool> selected: false;
                 x: it.grid-col * txt.preferred-width * 1.4; // FIXed: Just use the pre-count values
                 y: it.grid-row * 20px;
                 height: txt.preferred-height * 1.1;
                 width: txt.preferred-width * 1.1;
                 border-width: 1px;
+                background: white;
                 txt := Text {
-                    text: model[ind].uid ;
+                    text: model[ind].uid;
                     visible: true;
-                    color: touch.pressed ? red : black;
+                    color: it.selected ? red : black;
                 }
-                touch := TouchArea { }
+                touch := TouchArea { clicked => {
+                    if (it.selected) {
+                        it.selected = false;
+                        range-select-started-from = -1;
+                    } else {
+                        if (range-select-started-from == -1) {
+                            range-select-started-from = ind;
+                            it.selected = true;
+                        } else if (range-select-started-from != -1) {
+                            range-select(range-select-started-from, ind, true);
+                            range-select-started-from = -1;
+                        }
+                    }
+                }
+                                                }
                 states [
+                    //pressed when touch.pressed: { selected: true; }
                     mouse-over when touch.has-hover: {
                         rect.background: lightgrey;
-                    }
-                    mouse-not-over when !touch.has-hover: {
-                        rect.background: white;
                     }
                 ]
             }
@@ -62,7 +80,7 @@ thread_local! {
 }
 
 pub fn main() {
-    let handle = MainWindow::new();
+    let handle: MainWindow = MainWindow::new();
     let handle_weak = handle.as_weak();
     let handle_clone: slint::Weak<MainWindow> = handle_weak.clone();
     let timer = Timer::default();
@@ -71,10 +89,31 @@ pub fn main() {
     let mut col: i32 = 0;
     let max_growth = 5usize;
     create_tables();
+    handle_clone.unwrap().on_range_select(move |b: i32, e: i32, mode: bool|{
+        let model_handle: ModelRc<Data> = handle_clone.unwrap().get_model();
+        // range does not work this way so normalizing up selected
+        let range: RangeInclusive<usize>;
+        if b > e {
+            range= e as usize ..=b as usize;
+        } else {
+            range = b as usize ..=e as usize;
+        }
+        let model: &VecModel<Data> = model_handle.as_any().downcast_ref::<VecModel<Data>>().unwrap();
+        for i in range {
+            let data_maybe: Option<Data> = model.row_data(i as usize);
+            if let Some(mut data) = data_maybe {
+                data.selected = mode;
+                model.set_row_data(i as usize, data);
+            } else {
+                dbg!("failed update ind:", i);
+            }
+        }
+    });
+    let handle_clone: slint::Weak<MainWindow> = handle_weak.clone();
     timer.start(TimerMode::Repeated, std::time::Duration::from_millis(200), move || {
         let model_handle: ModelRc<Data> = handle_clone.unwrap().get_model();
         let model: &VecModel<Data> = model_handle.as_any().downcast_ref::<VecModel<Data>>().unwrap();
-        model.push(Data{ grid_col:col as i32, grid_row: row, uid: format!("{0:08x}", count).into()});
+        model.push(Data{ selected: false, grid_col:col as i32, grid_row: row, uid: format!("{0:08x}", count).into()});
         if count % max_growth == max_growth - 1 { row += 1; col = 0; }
         else { col += 1; }
         ticket_encoded(count);
